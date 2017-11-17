49a07e5841c2a161b1b16c3f112020f417916696
n-busuttil <nbusuttil@norsys.fr>
2017-11-17 16:01:59 +0100

    feat(site): refactor and clean

--- src/modules/header.js
+++ src/modules/header.js
@@ -7,53 +7,64 @@ import Head from './head';
 
 import './header.scss';
 
+
+export function isMobile() {
+  if (typeof window !== `undefined`) {
+    const sizeWindow = window.innerWidth;
+    if (sizeWindow <= 765) {
+      return true;
+    }
+    return false;
+  }
+}
+
 class Header extends Component {
   constructor() {
     super();
     if (typeof window !== `undefined`) {
       this.state = {
         menuFixed: window.pageYOffset,
-        width: window.innerWidth,
         menuOpened: false
       }
     }
   }
 
+  componentDidMount() {
+    if (typeof window !== `undefined`) {
+      window.scrollTo(0, 0)
+    }
+  }
+
   componentWillMount() {
     if (typeof window !== `undefined`) {
-      window.addEventListener('resize', this.handleWindowSizeChange);
       window.addEventListener('scroll', this.handleWindowScroll);
     }
   }
 
   componentWillUnmount() {
     if (typeof window !== `undefined`) {
-      window.removeEventListener('resize', this.handleWindowSizeChange);
       window.addEventListener('scroll', this.handleWindowScroll);
     }
   }
 
-  handleWindowSizeChange = () => {
-    if (typeof window !== `undefined`) {
-      this.setState({ width: window.innerWidth });
-    }
-  }
-
-  handleWindowScroll = () => {
+  handleWindowScroll = () =>{
     if (typeof window !== `undefined`) {
       this.setState({ menuFixed: window.pageYOffset });
     }
   }
 
+  handleClick() {
+    if (typeof window !== `undefined` && isMobile()) {
+      this.setState({menuOpened: !this.state.menuOpened});
+    }
+  }
+
   render() {
     if (typeof window !== `undefined`) {
-      const isMobile = this.state.width <= 765;
       const isFixed = this.state.menuFixed >= 300;
-      const params = () => this.setState({menuOpened: !this.state.menuOpened});
-
 
       const headerClass = classNames('header', {
-        'header--opened': this.state.menuOpened && isMobile === true,
+        'header--opened': this.state.menuOpened && isMobile(),
         'header--fixed': isFixed
       });
 
@@ -61,30 +72,30 @@ class Header extends Component {
         <div className="App">
           <Head />
           <div className={headerClass}>
-            <div className="header__menu-icons" onClick={params} />
+            <div className="header__menu-icons" onClick={this.handleClick.bind(this)} />
             <ul className="header__menu">
-              <li className="header__menu-item" onClick={params}>
+              <li className="header__menu-item" onClick={this.handleClick.bind(this)}>
                 <NavLink exact activeClassName="header__menu-item--selected" to="/">Accueil</NavLink>
               </li>
-              <li className="header__menu-item" onClick={params}>
+              <li className="header__menu-item" onClick={this.handleClick.bind(this)}>
                 <NavLink activeClassName="header__menu-item--selected" to="/psychologie">Qu'est-ce qu'un psychologue <span>clinicien ?</span></NavLink>
               </li>
-              <li className="header__menu-item" onClick={params}>
+              <li className="header__menu-item" onClick={this.handleClick.bind(this)}>
                 <NavLink activeClassName="header__menu-item--selected" to="/me-connaitre">Me connaître</NavLink>
               </li>
-              <li className="header__menu-item" onClick={params}>
+              <li className="header__menu-item" onClick={this.handleClick.bind(this)}>
                 <NavLink activeClassName="header__menu-item--selected" to="/pourquoi-consulter">Pourquoi consulter ?</NavLink>
               </li>
-              <li className="header__menu-item" onClick={params}>
+              <li className="header__menu-item" onClick={this.handleClick.bind(this)}>
                 <NavLink activeClassName="header__menu-item--selected" to="/qui-consulter">Qui consulter ?</NavLink>
               </li>
-              <li className="header__menu-item" onClick={params}>
+              <li className="header__menu-item" onClick={this.handleClick.bind(this)}>
                 <NavLink activeClassName="header__menu-item--selected" to="/therapie">Thérapie</NavLink>
               </li>
-              <li className="header__menu-item" onClick={params}>
+              <li className="header__menu-item" onClick={this.handleClick.bind(this)}>
                 <NavLink activeClassName="header__menu-item--selected" to="/tarifs">Tarifs</NavLink>
               </li>
-              <li className="header__menu-item" onClick={params}>
+              <li className="header__menu-item" onClick={this.handleClick.bind(this)}>
                 <NavLink activeClassName="header__menu-item--selected" to="/contacts">Contacts</NavLink>
               </li>
             </ul>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Link from 'gatsby-link';
 import Meta from '../modules/meta';
 
-import './home.css';
+import './home.scss';
 
 const metadata = {
   title: 'Psychologue Clinicienne | Morgane Laroche | Cordeliers, Lyon 2ème',
