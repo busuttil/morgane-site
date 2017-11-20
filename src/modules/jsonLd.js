5e52847b32daf3d6704d64b6ba55d9984ab2ca0b
n-busuttil <nbusuttil@norsys.fr>
2017-11-20 11:47:33 +0100

    feat(site): update microdata

--- src/modules/jsonLd.js
+++ src/modules/jsonLd.js
@@ -17,7 +17,11 @@ const JsonLd = () => {
     "sameAs": [
       "http://www.lyon-psychologue.com/",
       "https://fr.linkedin.com/in/morgane-laroche-a8700b138",
-      "https://plus.google.com/u/1/102102364543935278586"
+      "https://plus.google.com/u/1/102102364543935278586",
+      "https://www.psychologue.net/cabinets/morgane-laroche",
+      "https://annuaire.118712.fr/Rhone-69/Lyon-69002/Laroche-morgane-(psychologue-clinicienne)-0674318234_9E0020S00004F90300C90751S",
+      "http://www.psychologuelyon.com/psychologue/morgane-laroche-psychologue-clinicienne-a-lyon.html",
+      "https://www.yelp.fr/biz/morgane-laroche-lyon?osq=Psychologue"
     ]
   };
 
